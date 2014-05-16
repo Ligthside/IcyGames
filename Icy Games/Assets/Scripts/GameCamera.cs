f070b5100b16a1a701cf759c736d8c6b41a98753
ricardobarros95 <ricardobarros95@gmail.com>
2014-05-16 21:15:34 +0100

    Version 0.1
    Movement and Mouse pointing

---
+++ Icy Games/Assets/Scripts/GameCamera.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+
+	//public float distance = 5;
+	//public float height = 10;
+
+	private Vector3 cameraTarget;
+
+	private Transform player;
+	public Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+
+	}
+	
+	// Update is called once per frame
+//	void Update () {
+//
+//		cameraTarget = new Vector3(player.position.x, height, player.position.z);
+//		transform.position = Vector3.Lerp(transform.position, cameraTarget, Time.deltaTime * 8);
+//
+//
+//
+//	}
+
+	public Transform car;
+	public float distance = 15f;
+	public float height = 2.4f;
+	public float rotationDamping = 3.0f;
+	public float heightDamping = 2.0f;
+	public float zoomRatio = 0.5f;
+	public float defaultFOV = 60f;
+	private Vector3 rotationVector;
+	
+	void LateUpdate () {
+		
+		float wantedAngle = rotationVector.y;
+		float wantedHeight = car.position.y + height;
+		float myAngle = transform.eulerAngles.y;
+		float myHeight = transform.position.y;
+		myAngle = Mathf.LerpAngle(myAngle, wantedAngle, rotationDamping*Time.deltaTime);
+		myHeight = Mathf.Lerp(myHeight,wantedHeight, heightDamping*Time.deltaTime);
+		Quaternion currentRotation = Quaternion.Euler(0,myAngle,0);
+		transform.position = car.position;
+		transform.position -= currentRotation*Vector3.forward*distance;
+		Vector3 temp = transform.position;
+		temp -= currentRotation*Vector3.forward*distance;
+		temp.y = myHeight;
+		transform.position = temp;
+		transform.LookAt(car);
+		
+	}
+}
---
+++ Icy Games/Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof (CharacterController))]
+public class Movement : MonoBehaviour {
+
+	public float rotationSpeed = 450;
+	public float walkSpeed = 5;
+	public float runSpeed = 8;
+
+	private CharacterController controller;
+	private Camera cam;
+
+	private Quaternion targetRotation;
+
+	void Start(){
+		controller = GetComponent<CharacterController>();
+		cam = Camera.main;
+	}
+
+	void Update(){
+		characterMovement();
+	}
+	void characterMovement(){
+		Vector3 mousePos = Input.mousePosition;
+		mousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.transform.position.y - transform.position.y));
+		targetRotation = Quaternion.LookRotation(mousePos - new Vector3(transform.position.x,0,transform.position.z));
+		transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 motion = input;
+		motion *= (Mathf.Abs(input.x)== 1 && Mathf.Abs(input.z)== 1)?.7f:1;
+		motion *= (Input.GetButton("Run"))? runSpeed:walkSpeed;
+		motion += Vector3.up * -8;
+
+		controller.Move(motion * Time.deltaTime);
+	}
+}
+
+
+
+
+
+
+
